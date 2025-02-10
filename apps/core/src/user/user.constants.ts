6a5af3c783be0a0a63044482e6aaf81cbbcd425b
Papa_Carlo <rock_cell@mail.ru>
2025-02-10 15:14:41 +0100

    feat: discord binding flow refactor: assotiation with socials configuration

--- apps/core/src/user/user.constants.ts
+++ apps/core/src/user/user.constants.ts
@@ -3,6 +3,7 @@ import { IUser } from '../shared/interfaces';
 export enum ESocialPlatform {
   Twitter = 'twitter',
   Github = 'github',
+  Discord = 'discord',
   Telegram = 'telegram',
 }
 
@@ -10,4 +11,5 @@ export const SocialFieldMap: Record<ESocialPlatform, keyof IUser> = {
   [ESocialPlatform.Twitter]: 'twitter',
   [ESocialPlatform.Github]: 'github',
   [ESocialPlatform.Telegram]: 'telegram',
+  [ESocialPlatform.Discord]: 'discord',
 };
