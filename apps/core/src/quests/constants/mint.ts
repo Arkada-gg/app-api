8d2fcabb78895b543840875f18df7109d60cb31c
Papa_Carlo <rock_cell@mail.ru>
2025-03-12 10:18:28 +0100

    feat: added stage mint prices

--- apps/core/src/quests/constants/mint.ts
+++ apps/core/src/quests/constants/mint.ts
@@ -1,8 +1,14 @@
 import { parseEther } from 'ethers';
 import { ARKADA_NFTS } from '../../shared/constants/addresses';
 
-export const BASIC_QUEST_MINT_PRICE = parseEther('0.000075');
-export const PREMIUM_QUEST_MINT_PRICE = parseEther('0.00015');
+export const BASIC_QUEST_MINT_PRICE =
+  process.env.ENV === 'stage'
+    ? parseEther('0.000000075')
+    : parseEther('0.000075');
+export const PREMIUM_QUEST_MINT_PRICE =
+  process.env.ENV === 'stage'
+    ? parseEther('0.00000015')
+    : parseEther('0.00015');
 
 export const MAX_BPS = 10000; // 100%
 export const REF_OWNER_BPS = 1000; // 10%
