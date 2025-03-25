102555eaedb9faec47b6bfbeb1d1a3f4579404e1
Anton <anton@arkada.gg>
2025-03-25 14:50:42 +0300

    init sentry only not on development

--- apps/core/src/instrument.ts
+++ apps/core/src/instrument.ts
@@ -1,18 +1,20 @@
 import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    // Add our Profiling integration
-    nodeProfilingIntegration(),
-  ],
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // Add our Profiling integration
+      nodeProfilingIntegration(),
+    ],
 
-  // Add Tracing by setting tracesSampleRate
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0.05,
+    // Add Tracing by setting tracesSampleRate
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.05,
 
-  // Set sampling rate for profiling
-  // This is relative to tracesSampleRate
-  profilesSampleRate: 0.1,
-});
+    // Set sampling rate for profiling
+    // This is relative to tracesSampleRate
+    profilesSampleRate: 0.1,
+  });
+}
