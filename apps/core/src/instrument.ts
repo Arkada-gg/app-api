8823aa50c615b464186d83767635bec50670abb2
Anton <anton@arkada.gg>
2025-03-25 20:00:03 +0300

    feat: sentry instrument caches

--- apps/core/src/instrument.ts
+++ apps/core/src/instrument.ts
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { redisIntegration } from '@sentry/nestjs';
+import { CacheService } from './redis/cache.service';
 
 if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
@@ -7,6 +9,9 @@ if (process.env.NODE_ENV !== 'development') {
     integrations: [
       // Add our Profiling integration
       nodeProfilingIntegration(),
+      redisIntegration({
+        cachePrefixes: [CacheService.cachePrefix]
+      })
     ],
 
     // Add Tracing by setting tracesSampleRate
