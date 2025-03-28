bed79f18d92b4b23515302b6891a677e94b81713
Papa_Carlo <rock_cell@mail.ru>
2025-03-28 10:14:02 +0100

    fix: pyramid claim in one tx

--- apps/core/src/alchemy/alchemy.module.ts
+++ apps/core/src/alchemy/alchemy.module.ts
@@ -1,13 +1,14 @@
 // src/alchemy/alchemy.module.ts
 import { Module } from '@nestjs/common';
-import { UserModule } from '../user/user.module';
-import { TransactionsModule } from '../transactions/transactions.module';
-import { QuestModule } from '../quests/quest.module';
 import { _ConfigModule } from '../_config/config.module';
+import { DatabaseModule } from '../database/database.module';
+import { QuestModule } from '../quests/quest.module';
+import { AlchemyQueueModule } from '../queues/alchemy-queue.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { UserModule } from '../user/user.module';
+import { AlchemyCommonModule } from './alchemy-common.module';
 import { AlchemyWebhooksController } from './alchemy.controller';
 import { AlchemyWebhooksService } from './alchemy.service';
-import { AlchemyQueueModule } from '../queues/alchemy-queue.module';
-import { AlchemyCommonModule } from './alchemy-common.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AlchemyCommonModule } from './alchemy-common.module';
     TransactionsModule,
     QuestModule,
     _ConfigModule,
+    DatabaseModule,
   ],
   controllers: [AlchemyWebhooksController],
   providers: [AlchemyWebhooksService],
